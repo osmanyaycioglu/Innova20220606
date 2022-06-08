8cda74c9e12b90cde593958e5b53df8826a63d2e
osman <osman@osman123.com>
2022-06-08 12:21:59 +0300

    day 3 1

---
+++ node-configuration/src/main/java/com/training/innova/NodeConfigurationApplication.java
@@ -0,0 +1,15 @@
+package com.training.innova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class NodeConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeConfigurationApplication.class, args);
+	}
+
+}
